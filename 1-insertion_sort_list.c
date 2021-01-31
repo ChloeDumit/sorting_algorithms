de8058148b57b0ca17ca2c70fdd05711985fb877
Chloe <2106@holbertonschool.com>
2021-01-31 16:46:16 +0000

    betty fix

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -6,43 +6,43 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *current, *aux;
-    listint_t *temp;
+listint_t *current, *aux;
+listint_t *temp;
 
-    if (!list || *list == NULL)
-        return;
-    
-    current = *list;
-
-    if (current->next == NULL)
-        return;
-
-    while(current->next != NULL)
-    {
-        aux = current->next;
-
-        if (current->n > aux->n)
-        {
-            temp = current;
-            while(temp != NULL && temp->n > aux->n)
-            {
-                temp->next = aux->next;
-                if(temp->next != NULL)
-                    temp->next->prev = temp;
-                aux->prev = temp->prev;
-                if (aux->prev != NULL)
-                    aux->prev->next = aux;
-                else
-                {
-                    *list = aux;
-                }
-                temp->prev = aux;
-                aux->next = temp;
-                print_list(*list);
-                temp = aux->prev;    
-            }
-            continue;
-        }
-        current = current->next;
-    }
+if (!list || *list == NULL)
+return;
+current = *list;
+if (current->next == NULL)
+return;
+while (current->next != NULL)
+{
+aux = current->next;
+if (current->n > aux->n)
+{
+temp = current;
+while (temp != NULL && temp->n > aux->n)
+{
+temp->next = aux->next;
+if (temp->next != NULL)
+{
+temp->next->prev = temp;
+}
+aux->prev = temp->prev;
+if (aux->prev != NULL)
+{
+aux->prev->next = aux;
+}
+else
+{
+*list = aux;
+}
+temp->prev = aux;
+aux->next = temp;
+print_list(*list);
+temp = aux->prev;
+}
+continue;
+}
+current = current->next;
+}
 }
