f2fe3511dda494f786d89d789c58ac1514a848b1
Chloe <2106@holbertonschool.com>
2021-01-31 16:49:07 +0000

    betty fix

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -7,27 +7,26 @@
  */
 void selection_sort(int *array, size_t size)
 {
-    size_t position = 0, first = 0, min = 0;
-    int temp = 0;
+size_t position = 0, first = 0, min = 0;
+int temp = 0;
 
-        for (first = 0; first < size; first++)
-        {
-            min = first;
+for (first = 0; first < size; first++)
+{
+min = first;
 
-            for(position = first + 1; position < size; position++)
-            {
-                if (array[position] < array[min])
-                {
-                    min = position;
-                    
-                }           
-            }
-            if (min != first)
-            {
-                    temp = array[first];
-                    array[first] = array[min];
-                    array[min] = temp;
-                    print_array(array, size);
-            }
-        } 
-} 
+for (position = first + 1; position < size; position++)
+{
+if (array[position] < array[min])
+{
+min = position;
+}
+}
+if (min != first)
+{
+temp = array[first];
+array[first] = array[min];
+array[min] = temp;
+print_array(array, size);
+}
+}
+}
