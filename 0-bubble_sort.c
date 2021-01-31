d345cd4a7fd30e78fcc0a7616d0ac6d23254a5be
Chloe <2106@holbertonschool.com>
2021-01-31 16:40:23 +0000

    betty fix

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,23 +7,20 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, j;
-    int tmp;
+size_t i, j;
+int tmp;
 
-    for(j = 0; j < size; j++)
-    {
-        for(i=0; i < size - 1; i++)
-        {
-            if (array[i] > array[i+1])
-            {
-                tmp = array[i];
-                array[i] = array[i+1];
-                array[i+1] = tmp;
-                print_array(array, size); 
-            }
-            
-                  
-        }    
-        
-    }
+for (j = 0; j < size; j++)
+{
+for (i = 0; i < size - 1; i++)
+{
+if (array[i] > array[i + 1])
+{
+tmp = array[i];
+array[i] = array[i + 1];
+array[i + 1] = tmp;
+print_array(array, size);
+}
+}
+}
 }
